a52917cdc9fdc601d9a7ef601bf6012bc9bc2707
Miko Suria <miko.suria@gmail.com>
2022-12-11 02:29:35 +0800

    removed unused files

--- src/components/common/ScheduleComponent.js
+++ src/components/common/ScheduleComponent.js
@@ -4,8 +4,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-//import '../../css/schedule.css'
-
 const ScheduleComponent = (props) => {
 
     const colBreakpointClass = (idx) => {
